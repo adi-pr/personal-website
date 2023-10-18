0dd315440e0c1f6173ba172801f56540ac0ddc9e
Ruben <adityaprasad7123@gmail.com>
2023-10-18 14:13:55 -0400

    Added Contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { unica_one } from "@/constants";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section id="contact" className="bg-[#191923] md:px-20 py-5">
+      <div className="flex justify-between items-center px-3">
+        <div className="bg-white w-full h-[5px]" />
+        <h2
+          className={`${unica_one.className} text-white text-[30px] lg:text-[60px] md:text-[45px] m-5`}
+        >
+          Contact
+        </h2>
+        <div className="bg-white w-full h-[5px]" />
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
