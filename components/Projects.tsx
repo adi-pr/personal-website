ecdab15bc5a9cf8f045265fc98bb4c66ebe2b144
Ruben <adityaprasad7123@gmail.com>
2023-10-18 20:43:46 -0400

    Revert "Revert "Added more projects and updated styling""
    This reverts commit dad8cdbafb5046107a5b6e178fd6adf3f6c84415.

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,10 @@
-"use client"
+"use client";
 
 import { useRef, useState, useEffect } from "react";
-import Lottie, {LottieRefCurrentProps} from "lottie-react";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
 import animationData from "../public/animation_lnqx659d.json";
+
+import { projectData } from "@/constants";
 
 const ArrowAnimation = ({ onHover }: { onHover: boolean }) => {
   const arrowRef = useRef<LottieRefCurrentProps>(null);
@@ -18,26 +20,37 @@ const ArrowAnimation = ({ onHover }: { onHover: boolean }) => {
 
   return (
     <div className="-rotate-90 w-[70px] h-[70px]">
-      <Lottie
-      loop={false}
-        lottieRef={arrowRef}
-        animationData={animationData}
-      />
+      <Lottie loop={false} lottieRef={arrowRef} animationData={animationData} />
     </div>
   );
 };
 
-const ProjectWrapper = ({ value }: { value: number }) => {
+const ProjectWrapper = ({
+  value,
+  name,
+  link,
+}: {
+  value: number;
+  name: string;
+  link: string;
+}) => {
   const [onHover, setOnHover] = useState(false);
 
   return (
-    <div
-      className="flex justify-between items-center border-2 p-3 rounded-lg z-0 bg-[#191923] text-white w-full"
-      onMouseEnter={() => setOnHover(true)}
-      onMouseLeave={() => setOnHover(false)}
-    >
-      <p>Project {value}</p>
-      <ArrowAnimation onHover={onHover} />
+    <div className="z-0 md:w-[500px]">
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <div
+          className="flex justify-between items-center border-[1px] p-3 rounded-xl  bg-[#191923] text-white w-full"
+          onMouseEnter={() => setOnHover(true)}
+          onMouseLeave={() => setOnHover(false)}
+        >
+          <div>
+            <p className="text-xl">Project {value}</p>
+            <p className="text-sm text-gray-400">{name}</p>
+          </div>
+          <ArrowAnimation onHover={onHover} />
+        </div>
+      </a>
     </div>
   );
 };
@@ -45,9 +58,14 @@ const ProjectWrapper = ({ value }: { value: number }) => {
 const Projects = () => {
   return (
     <section className="bg-[#191923] md:px-20 py-5 w-full z-50">
-      <div className="p-4 md:p-8 flex flex-col gap-5 h-full w-full text-white">
-        {[1, 2, 3].map((projectNumber) => (
-          <ProjectWrapper key={projectNumber} value={projectNumber} />
+      <div className="p-4 md:p-8 flex flex-wrap md:flex-row justify-between flex-col gap-5 h-full w-full text-white">
+        {projectData.map((project) => (
+          <ProjectWrapper
+            key={project.number}
+            value={project.number}
+            name={project.name}
+            link={project.github}
+          />
         ))}
       </div>
     </section>
